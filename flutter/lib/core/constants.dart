5aa2775cb6f984d6e5dfef1dbcde76a7a9c1c7ab
Loxis88 <grisham436@gmail.com>
2026-06-10 16:06:34 +0300

    ci(flutter): configure android build with secrets and debug signing
    - Add secret injection via String.fromEnvironment for constants
    - Update deploy.yml to decode keystore and pass flags via dart-define
    - Maintain debug build for Flutter APK

--- flutter/lib/core/constants.dart
+++ flutter/lib/core/constants.dart
@@ -1,2 +1,2 @@
-const coconutBackendBaseUrl = 'http://62.233.43.33:8080';
-const googleClientId = '810958888238-c1fmanoapbjbgha6nkbte55o99cqj446.apps.googleusercontent.com';
+const coconutBackendBaseUrl = String.fromEnvironment('BACKEND_URL', defaultValue: 'http://62.233.43.33:8080');
+const googleClientId = String.fromEnvironment('GOOGLE_CLIENT_ID', defaultValue: '810958888238-c1fmanoapbjbgha6nkbte55o99cqj446.apps.googleusercontent.com');
